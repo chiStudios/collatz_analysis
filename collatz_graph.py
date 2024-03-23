3fb81399c5125a9830a4edfbd1ecc433f9a0de76
cake <3rdbinarybug@gmail.com>
2024-03-23 11:03:38 -0400

    Update collatz_graph.py
    code cleaning

--- collatz_graph.py
+++ collatz_graph.py
@@ -1,5 +1,3 @@
-# import csv
-# from functools import cache
 import matplotlib.pyplot as plt
 import time
 from sys import setrecursionlimit
@@ -9,7 +7,6 @@ class cg():
     def __init__(self):
         self.global_cache = {}
 
-    # @cache
     def collatz(self, x, step=0):
         if x in self.global_cache.keys():  
             return step + self.global_cache[x]
@@ -30,7 +27,6 @@ class cg():
         for x in range(x, rng):
             step = self.collatz(x)
             y_points.append(step) # conjecture recursion function
-            # if new_path: paths += 1
             if not x in cache.keys(): self.global_cache[x] = step
 
         return y_points
@@ -39,10 +35,9 @@ class cg():
         print(f'Running calculations for {start_val:,} through {stop_val:,}...')
         start_time = time.time()
         y_points = self.run_collatz(start_val, stop_val, self.global_cache)
-        stop_time = time.time()
-        calc_time = stop_time - start_time
+        calc_time = time.time() - start_time
         x_points = [x for x in range(start_val, stop_val)]
-        print(f'Took {calc_time} seconds...')
+        print(f'Took {calc_time:.2f} seconds...')
 
         print('Dumping cache...')
         self.global_cache.clear()
@@ -51,39 +46,12 @@ class cg():
         plt.plot(x_points, y_points, marker=',', linestyle='')
         plt.xlabel('Loop Start Value') # set X axis label on graph
         plt.ylabel('Loop Range') # set Y axis label on graph
-        # plt.xscale('log')
-        plt.title(f'Collatz Conjecture | Plotting {start_val:,} to {stop_val:,} | Calculations took {calc_time:.2f} secs')
+        plt.title(f'Collatz Conjecture | Plotting {start_val:,} to {stop_val:,} | Calculations took {calc_time:.1f} secs')
 
-        # print('Saving plot...')
-        # plt.savefig(f'Collatz_Array_{start_val}_{stop_val}.png') # save graph to png
+        print('Saving plot to image...')
+        plt.savefig(f'Collatz_Array_{start_val}_{stop_val}_{calc_time:.0f}secs.png') # save graph to png
 
         print('Displaying plot...')
         plt.show() # show graph in gui
 
-    # def plot_step_freq(self, start_val, stop_val):
-    #     print(f'Running calculations for {start_val:,} through {stop_val:,}...')
-    #     x_points = self.run_collatz(start_val, stop_val, self.global_cache)
-    #     y_points = [y for y in range(start_val, stop_val)]
-
-    #     print('Plotting coordinates...')
-    #     plt.plot(x_points, y_points, marker=',', linestyle='')
-    #     plt.title(f'Collatz Conjecture Step Frequency | Plotting {start_val:,} to {stop_val:,}')
-    #     plt.xlabel(f'X')
-    #     plt.ylabel('Number of steps for X to reach 1')
-
-    #     print('Saving plot...')
-    #     plt.savefig(f'Collatz__Step_Freq_Array_{start_val}_{stop_val}.png') # save graph to png
-
-
-
-# cg().plot_step_freq(1, 100_000) 
-cg().plot_points(1, 100_000_000) # for stable execution do not exceed a 1,000,000 unit gap
-
-# def multi_plot(multiplier, start_val, stop_val):
-#     addition = stop_val
-#     for x in range(multiplier):
-#         plot_points(start_val, stop_val)
-#         start_val = stop_val + 1
-#         stop_val += addition
-
-# multi_plot(10, 1, 1_000_000)
+cg().plot_points(1, 10_000_000) # for stable execution do not exceed a 1,000,000 unit gap
